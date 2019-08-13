d8032f554f1a34476e3d6355edeb8faca23ee8b9
PanamcheG <yashkovich94@mail.ru>
2019-08-13 21:49:07 +0300

    Task 2 Lesson 0

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,12 +3,12 @@ import org.junit.Test;
 public class MainClassTest
 {
     @Test
-    public void testGetLocalNumber (){
-        if (MainClass.getLocalNumber() == 14) {
+    public void testGetClassNumber (){
+        if (MainClass.getClassNumber() > 45) {
             System.out.println("Test successful!");
         }
             else    {
-                System.out.println("Local number is not 14!");
+                System.out.println("Class number is less that 45!");
             }
     }
 }
